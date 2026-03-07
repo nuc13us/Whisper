743e05cf1c9746588e064c55eb976746c5a7c232
Rakesh Paruchuri <nuc13us@users.noreply.github.com>
2026-03-07 13:35:28 +0530

    Add ApolloServer examples for schemaDirectives usage
    Added examples of ApolloServer configurations with and without schemaDirectives.

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -2,6 +2,24 @@ const crypto = require('crypto');
 
 // Defining separate email validation middleware
 const validator = require('validator').default;
+
+// BAD: Has 'schemaDirectives'
+//ruleid: schema-directives
+const apollo_server_1 = new ApolloServer({
+    typeDefs,
+    resolvers,
+    schemaDirectives: {
+        rateLimit: rateLimitDirective
+    },
+});
+
+// Good: Does not have 'schemaDirectives'
+//ok: schema-directives
+const apollo_server_3 = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
 const emailValidator = (req, res, next) => {
   const { email } = req.body;
 
