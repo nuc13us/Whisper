7db23bb667ab22eaf9ebfd1ae9ee00c8f1a001e1
Rakesh Paruchuri <nuc13us@users.noreply.github.com>
2026-03-07 23:07:42 +0530

    Add comment for testing in app initialization
    Added a comment to the app initialization line for testing purposes.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
+const app = express(); // testing
 const http = require('http');
 const server = http.createServer(app);
 const HTTP_PORT = process.env.PORT || 4000;
